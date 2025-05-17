bd1592cb02457f742d8a7cffd20cc55b3966c902
levibmackay <mac21048@byui.edu>
2025-05-16 19:41:03 -0600

    Update

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -6,3 +6,46 @@ function toggleMenu() {
 }
 
 menuButton.addEventListener('click', toggleMenu);
+
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+}
+
+window.addEventListener("resize", handleResize);
+window.addEventListener("load", handleResize);
+
+const gallery = document.querySelector(".gallery");
+const modal = document.createElement("dialog");
+document.body.appendChild(modal);
+
+gallery.addEventListener("click", (event) => {
+  const img = event.target.closest("img");
+  if (!img) return;
+
+  const src = img.getAttribute("src");
+  const alt = img.getAttribute("alt");
+  const newSrc = src.split("-")[0] + "-full.jpeg";
+
+  //This is where I got lost.
+  modal.innerHTML = `
+    <img src="${newSrc}" alt="${alt}">
+    <button class="close-viewer">X</button>
+  `;
+
+  modal.showModal();
+
+  const closeBtn = modal.querySelector(".close-viewer");
+  closeBtn.addEventListener("click", () => {
+    modal.close();
+  });
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
