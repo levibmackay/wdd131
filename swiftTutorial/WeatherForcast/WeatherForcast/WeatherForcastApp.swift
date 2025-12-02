74215c141d8d0099580479d5846837cc9c3fcc73
Levi Mackay <mac21048@byui.edu>
2025-12-02 10:32:42 -0700

    update

---
+++ swiftTutorial/WeatherForcast/WeatherForcast/WeatherForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastApp.swift
+//  WeatherForcast
+//
+//  Created by Levi Mackay on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftTutorial/swiftTutorial/swiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTutorialApp.swift
+//  swiftTutorial
+//
+//  Created by Levi Mackay on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
